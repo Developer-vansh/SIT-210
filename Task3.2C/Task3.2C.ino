b042aa7aa29580b326b0d15691442fe175edb33c
Developer-vansh <117535964+Developer-vansh@users.noreply.github.com>
2023-09-01 17:00:14 +0530

    Update Task3.2C.ino

--- Task3.2C/Task3.2C.ino
+++ Task3.2C/Task3.2C.ino
@@ -5,66 +5,64 @@ char ssid[] = "iPhone";
 char pass[] = "12345666";
 
 WiFiClient client;
-
-char   HOST_NAME[] = "maker.ifttt.com";
-String PATH_NAME   = "/trigger/SunLightPresent/with/key/phqmslABUAaVa7weS_RRqc97mgk6UXsI93QdH4qzLjV"; // change your EVENT-NAME and YOUR-KEY
-const int SensorPin = A0; 
-bool prevSunlightState = false;
+const int ldrPin = A0;
+char HOST_NAME[] = "maker.ifttt.com";                                                                //Set the host name for IFTTT
+String PATH_NAME = "/trigger/SunLightPresent/with/key/phqmslABUAaVa7weS_RRqc97mgk6UXsI93QdH4qzLjV";  // Set the path for sunlight event
+bool previousSunlightState = false;
 
 void setup() {
   // initialize WiFi connection
   WiFi.begin(ssid, pass);
 
-  Serial.begin(9600);
-  while (!Serial);
+  Serial.begin(9600);  // Start serail communication
+  while (!Serial)
+    ;
 
-  // connect to web server on port 80:
+  // Connect to web server on port 80:
   if (client.connect(HOST_NAME, 80)) {
     // if connected:
     Serial.println("Connected to server");
-  }
-  else {// if not connected:
+  } else {  // if not connected:
     Serial.println("connection failed");
   }
-
 }
-
 void loop() {
-  int Sensorvalue = analogRead(SensorPin);  // Read analog value from LDR sensor
-  Serial.println(Sensorvalue);  // Print the Sensor value to the monitor
+  check_sensor_value();
+}
+void check_sensor_value() {
+  int Sensorvalue = analogRead(ldrPin);  // Read analog value from LDR sensor
+  Serial.println(Sensorvalue);           // Print the Sensor value to the monitor
 
-  bool currSunlightState = Sensorvalue < 100;  // Determine if it's currently sunlight based on LDR value threshold
+  bool currSunlightState = Sensorvalue < 100;  // Determine if  sunlight present based on  threshold for LDR value
 
-  if (currSunlightState != prevSunlightState) {
-    prevSunlightState = currSunlightState;
+  if (currSunlightState != previousSunlightState) {
+    previousSunlightState = currSunlightState;
 
     if (currSunlightState) {
       //  Is sunlight present then send HTTP request for sunlight:
       httpRequest(PATH_NAME);  // Call function to send HTTP request for sunlight event
-    } 
+    }
   }
   delay(500);  // Delay for 1/2 second before the next iteration
 }
-void httpRequest(String PATH_NAME){
+void httpRequest(String PATH_NAME) {
+  // make a HTTP request:
+  // send HTTP header
+  client.println("GET " + PATH_NAME + " HTTP/1.1");
+  client.println("Host: " + String(HOST_NAME));
+  client.println("Connection: close");
+  client.println();  // end HTTP header
 
-    // make a HTTP request:
-    // send HTTP header
-    client.println("GET " + PATH_NAME + " HTTP/1.1");
-    client.println("Host: " + String(HOST_NAME));
-    client.println("Connection: close");
-    client.println(); // end HTTP header
-
-    while (client.connected()) {
-      if (client.available()) {
-        // read an incoming byte from the server and print it to serial monitor:
-        char c = client.read();
-        Serial.print(c);
-      }
+  while (client.connected()) {
+    if (client.available()) {
+      // read an incoming byte from the server and print it to serial monitor:
+      char c = client.read();
+      Serial.print(c);
     }
+  }
 
-    // the server's disconnected, stop the client:
-    client.stop();
-    Serial.println();
-    Serial.println("disconnected");
-
+  // the server's disconnected, stop the client:
+  client.stop();
+  Serial.println();
+  Serial.println("disconnected");
 }
