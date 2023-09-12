b48b738a7de92ac463e714dd445a02881c7c42b8
Developer-vansh <117535964+Developer-vansh@users.noreply.github.com>
2023-09-12 18:04:31 +0530

    Task5.2_Gui.py

--- task-5.2/Task5.2_Gui.py
+++ task-5.2/Task5.2_Gui.py
@@ -21,53 +21,53 @@ GPIO.setup(green, GPIO.OUT)
 GPIO.setup(blue, GPIO.OUT)
 
 # Function to turn off all LEDs
-def turnOff():
+def LEDsOFF():
     GPIO.output(red, GPIO.LOW)
     GPIO.output(green, GPIO.LOW)
     GPIO.output(blue, GPIO.LOW)
 
 # Function to turn on the red LED
-def RedLight():
-    turnOff()  # Turn off all LEDs first
+def OnRedLED():
+    LEDsOFF()  # Turn off all LEDs first
     GPIO.output(red, GPIO.HIGH)  # Turn on the red LED
     print("Turn on red LED")
 
 # Function to turn on the green LED
-def GreenLight():
-    turnOff()  # Turn off all LEDs first
+def OnGreenLED():
+    LEDsOFF()  # Turn off all LEDs first
     GPIO.output(green, GPIO.HIGH)  # Turn on the green LED
     print("Turn on green LED")
 
 # Function to turn on the blue LED
-def BlueLight():
-    turnOff()  # Turn off all LEDs first
+def OnBlueLED():
+    LEDsOFF()  # Turn off all LEDs first
     GPIO.output(blue, GPIO.HIGH)  # Turn on the blue LED
     print("Turn on blue LED")
 
 # Function to close the GUI window and clean up GPIO settings
 def close():
-    turnOff()  # Turn off all LEDs
+    LEDsOFF()  # Turn off all LEDs
     GPIO.cleanup()  # Clean up GPIO settings
     window.destroy()  # Destroy the tkinter window
 
 # Create a tkinter window and set its title
 window = tk.Tk()
-window.title('LED Control')
+window.title('Vansh LED Control')
 
 # Created a button for turning on the red LED 
-button = tk.Button(window, text='Turn RED', command=RedLight, bg='red', height=1, width=50)
+button = tk.Button(window, text='Turn RED', command=OnRedLED, bg='red', height=1, width=40)
 button.pack()
 
 # Created a button for turning on the green LED 
-button = tk.Button(window, text='Turn GREEN', command=GreenLight, bg='green', height=1, width=50)
+button = tk.Button(window, text='Turn GREEN', command=OnGreenLED, bg='green', height=1, width=40)
 button.pack()
 
 # Created a button for turning on the blue LED 
-button = tk.Button(window, text='Turn BLUE', command=BlueLight, bg='blue', height=1, width=50)
+button = tk.Button(window, text='Turn BLUE', command=OnBlueLED, bg='blue', height=1, width=40)
 button.pack()
 
 # Created an exit button to close the application 
-exitbutton = tk.Button(window, text='Exit', command=close, bg='yellow', height=1, width=50)
+exitbutton = tk.Button(window, text='Exit', command=close, bg='yellow', height=2, width=30)
 exitbutton.pack()
 
 # Start the tkinter main event loop
